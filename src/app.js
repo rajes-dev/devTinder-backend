df76e16079641240928cc09f3227aca10c1f89c8
rajes-dev <159983841+rajes-dev@users.noreply.github.com>
2026-01-09 13:52:03 +0530

    database init

--- src/app.js
+++ src/app.js
@@ -1,19 +1,36 @@
 const express = require("express");
-const {DbConnect} = require('./config/database');
+const { DbConnect } = require("./config/database");
 const app = express();
+const user = require("./models/user");
 
-const {auth} = require('./middlewares/auth');
+const { auth } = require("./middlewares/auth");
 
-DbConnect().then(()=>{
-     console.log('connection established successfull');
-     app.listen(7777, ()=>{
-    console.log('listening on 7777')
+app.post("/signup", async (req, res) => {
+  const User = new user({
+    firstName: "Rajesh",
+    lastName: "Jami",
+    emailId: "raj@gmail.com",
+    password: "raj@jami",
+  });
+
+  try {
+    await User.save();
+    console.log("user created successsfully");
+    res.send("user creataed success");
+  } catch (err) {
+    res.status(400).send('user not created:'+ err.message)
+  }
 });
-}).catch((err)=>{
-  console.log('database connection not established')
-})
 
+DbConnect()
+  .then(() => {
+    console.log("connection established successfull");
+    app.listen(7777, () => {
+      console.log("listening on 7777");
+    });
+  })
+  .catch((err) => {
+    console.log("database connection not established");
+  });
 
 // mongodb+srv://namastedev:QwRD0dQ47FArzY04@namastenode.tqtnu8y.mongodb.net/
-
-
